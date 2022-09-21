a119e03fc0f34de930da782d7f6483ad39ebcecf
Guvi-Assignment-day-21 <krittika489podder@gmail.com>
2022-09-22 02:41:49 +0530

    skills done

--- src/Portfolio/Home/Home.js
+++ src/Portfolio/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Profile from './Profile'
 import About from '../About/About'
+import Skills from '../Skills/Skills'
 import './Home.css'
 
 export default function Home(){
@@ -8,6 +9,7 @@ export default function Home(){
         <div className='home-container'>
            <Profile/> 
            <About/> 
+           <Skills/>
         </div>
     )
 }
---
+++ src/Portfolio/Skills/Skills.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./Skills.css";
+export default function Skills(){
+  
+    return(
+<div className='skillset-container'>
+        <h1 className='skill-heading'>TECHNICAL SKILLS</h1>
+      <div className='skills-container'>
+      <img src={require('../../Assets/Skillspic/javascript.png')} alt="javascript" />
+      <img src={require('../../Assets/Skillspic/html.jpg')} alt="html" />
+      <img src={require('../../Assets/Skillspic/css.png')} alt="css" />
+      <img src={require('../../Assets/Skillspic/bootstrap.png')} alt="bootstrap" />
+      <img src={require('../../Assets/Skillspic/react.png')} alt="react js" />
+      <img src={require('../../Assets/Skillspic/mysql.jpg')} alt="mysql" />
+      <img src={require('../../Assets/Skillspic/mongodb.webp')} alt="mongodb" />
+      <img src={require('../../Assets/Skillspic/nodejs.jpg')} alt="node js" />
+      <img src={require('../../Assets/Skillspic/aws.png')} alt="aws" />
+      <img src={require('../../Assets/Skillspic/cprogramming.png')} alt="c programming" />
+      </div>
+<h1 className='tools-heading'>TOOLS AND METHODS</h1>  
+<div className='tools-container'>
+<img src={require('../../Assets/Skillspic/netlify.png')} alt="netlify" />
+<img src={require('../../Assets/Skillspic/heroku.png')} alt="heroku" />
+<img src={require('../../Assets/Skillspic/postman.png')} alt="postman" />
+</div>
+</div>
+    );
+}
