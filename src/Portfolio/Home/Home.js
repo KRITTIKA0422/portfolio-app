dad7373f97407b52ecbe545224cd82f79c73123b
Guvi-Assignment-day-21 <krittika489podder@gmail.com>
2022-09-23 10:28:55 +0530

    hands on lab done

--- src/Portfolio/Home/Home.js
+++ src/Portfolio/Home/Home.js
@@ -3,6 +3,7 @@ import Profile from './Profile'
 import About from '../About/About'
 import Skills from '../Skills/Skills'
 import Projects from '../Projects/Projects'
+import HandsOnLab from '../HandsOnLab/HandsOnLab'
 import './Home.css'
 
 export default function Home(){
@@ -12,6 +13,7 @@ export default function Home(){
            <About/> 
            <Skills/>
            <Projects/>
+           <HandsOnLab/>
         </div>
     )
 }
