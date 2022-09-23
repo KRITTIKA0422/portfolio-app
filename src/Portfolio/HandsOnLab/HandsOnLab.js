b5988026d2a40bbf22e4d151f7d7e7383aac5e66
Guvi-Assignment-day-21 <krittika489podder@gmail.com>
2022-09-23 10:28:41 +0530

    hands on lab

---
+++ src/Portfolio/HandsOnLab/HandsOnLab.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./HandsOnLab.css";
+export default function HandsOnLab(){
+    return(
+<div className='experience-container'>
+    <h1>HANDS ON LAB</h1>
+    <ul type="square" className='ul-container'>
+        <li>Experience during my coursework includes basic conceptual clarity of JavaScript, CSS and HTML.</li>
+        <li>Have done 60+ programs to develop logics and programming concepts</li>
+        <li>Designed responsive webpages like HTML forms, GitHub profile page, pagination using DOM, HTML, CSS and Bootstrap.</li>
+        <li>Fetched data from API servers using fetch and asynchronized functions.</li>
+        <li>Designed webpages using React.js and designed DB Models using SQL.</li>
+        <li>Can do queries for SQL and MongoDB.</li>
+        <li>Can fetch data from MongoDB using Atlas, create express server and create API in Node JS and can perform CRUD operations also.</li>
+        <li>Confident in frontend, backend development and deploying sites using Netlify and Heroku.</li>
+    </ul>
+</div>
+    );
+}
