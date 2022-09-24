36e87961c85adaf5c625ac21201e57c14a1c20cc
Guvi-Assignment-day-21 <krittika489podder@gmail.com>
2022-09-24 10:53:37 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+
 import Home from "./Portfolio/Home/Home";
+import About from './Portfolio/About/About';
+import Skills from './Portfolio/Skills/Skills';
+import Projects from "./Portfolio/Projects/Projects";
+import HandsOnLab from "./Portfolio/HandsOnLab/HandsOnLab";
+import Interests from "./Portfolio/Interests/Interests";
+import Contact from "./Portfolio/Contact/Contact";
+import { Routes, Route, Link } from "react-router-dom";
+import './App.css';
 
 function App(){
     return(
         <div className="App">
-        <Home />
+            <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={ <Skills/>} />
+        <Route path="/projects" element={<Projects/>} />
+        <Route path="/experience" element={ <HandsOnLab/>} />
+        <Route path="/interests" element={<Interests/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes> 
         </div>
     );
 }
--- src/Portfolio/Home/Home.js
+++ src/Portfolio/Home/Home.js
@@ -1,23 +1,11 @@
 import React from 'react'
 import Profile from './Profile'
-import About from '../About/About'
-import Skills from '../Skills/Skills'
-import Projects from '../Projects/Projects'
-import HandsOnLab from '../HandsOnLab/HandsOnLab'
-import Interests from '../Interests/Interests'
-import Contact from '../Contact/Contact'
 import './Home.css'
 
 export default function Home(){
     return(
         <div className='home-container'>
            <Profile/> 
-           <About/> 
-           <Skills/>
-           <Projects/>
-           <HandsOnLab/>
-           <Interests/>
-           <Contact/>
         </div>
     )
 }
